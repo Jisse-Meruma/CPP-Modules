662b53b595d8304fc83e8cd8125953261ab03491
Jisse Meruma <jissemeruma@gmail.com>
2023-11-10 13:31:16 +0100

    scarlar conversion

--- module-06/ex00/ScalarConverte.cpp
+++ module-06/ex00/ScalarConverte.cpp
@@ -2,6 +2,13 @@
 #include <string>
 #include <sstream>
 #include <limits>
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+ 
+#define INT 0
+#define DOUBLE 1
+#define FLOAT 2
 
 // Constructors and Destructor
 // ScalarConverte::ScalarConverte() {
@@ -28,8 +35,100 @@
 //     *this = obj;
 // }
 
+int     is_valid_digit(const std::string &input, const char *str, int *type)
+{
+    size_t i = 0;
+
+    while(isdigit(str[i]) && str[i])
+        i++;
+    if (str[i] == '.' && isdigit(str[i + 1]))
+    {
+        i++;
+        *type = DOUBLE;
+    }
+    while(isdigit(str[i]) && str[i])
+        i++;
+    if (str[i] == 'f')
+    {
+        i++;
+        *type = FLOAT;
+    }
+    if (input.length() == i)
+        return (1);
+    std::cerr << "Not a Valid Digit!" << std::endl;
+    return (0);
+}
+
+bool    int_overflow(long double number)
+{
+    if (number < std::numeric_limits<int>::max() && number > std::numeric_limits<int>::min())
+        return (true);
+    return (false);
+}
+
+bool    float_overflow(long double number)
+{
+    if (number < std::numeric_limits<double>::max() && number > std::numeric_limits<double>::min())
+        return (true);
+    return (false);
+}
+
+bool    double_overflow(long double number)
+{
+    if (number < std::numeric_limits<float>::max() && number > std::numeric_limits<float>::min())
+        return (true);
+    return (false);
+}
+
+void    ft_print_char(char charactar)
+{
+    std::cout << "Char: " << charactar << std::endl;
+    std::cout << "Int: " << static_cast<int>(charactar) << std::endl;
+    std::cout << "Float: " << static_cast<float>(charactar) << std::endl;
+    std::cout << "Double: " << static_cast<double>(charactar) << std::endl;
+}
+
+void    ft_print_int(long double number)
+{
+    if (int_overflow(number))
+    {
+        if (!isprint(number))
+            std::cout << "Char: " << static_cast<char>(number) << std::endl;
+        else
+            std::cout << "Non Displayable" << std::endl;
+        std::cout << "Int: " << static_cast<int>(number) << std::endl;
+    }
+    else
+    {
+        std::cout << "Char: Impossible" << std::endl;
+        std::cout << "Int: Impossible" << std::endl;
+    }
+    if (float_overflow(number))
+        std::cout << "Float: " << static_cast<float>(number) << 'f' << std::endl;
+    else
+        std::cout << "Float Impossible" << std::endl;
+    if (double_overflow(number))
+        std::cout << "Double: " << static_cast<double>(number) << std::endl;
+    else
+        std::cout << "Double Impossible" << std::endl;
+}
+
 
 void    ScalarConverte::convert(const std::string &input)
 {
-    std::typ
+    long double number;
+    int         type = INT;
+    const char  *str = input.c_str();
+
+    if (input.length() == 1 && isprint(str[0]) && !isdigit(str[0]))
+    {
+        ft_print_char(str[0]);
+    }
+    if (!is_valid_digit(input, str, &type))
+    {
+        return ;
+    }
+    sscanf(str, "%Lf", &number);
+    ft_print_int(number);
+
 }
--- module-06/ex00/main.cpp
+++ module-06/ex00/main.cpp
@@ -5,9 +5,11 @@ int	main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		std::cout << "Please Give 1 Argument to the program" << std::endl;
+		return (1);
 	}
 
 	std::string input(argv[1]);
 
 	ScalarConverte::convert(input);
+	return (0);
 }
